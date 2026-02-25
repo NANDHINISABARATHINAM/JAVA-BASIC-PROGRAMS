b1f827a1c766708c821f109fa9bcac998017d87f
NANDHINI_S02 <112407334+NANDHINISABARATHINAM@users.noreply.github.com>
2026-02-25 17:57:45 +0530

    Update EO2.java

--- EVEN ODD/EO2.java
+++ EVEN ODD/EO2.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if((n|1)>n){
+		    System.out.print("even");
+		}
+		else{
+		    System.out.print("odd");
+		}
+	}
+}
